4c07fb23bd3b2def1d9fff824638d9595cf8c7fb
Nik Klimenko <nikklimenko89@gmail.com>
2023-07-11 13:50:29 +0300

    feat: added pagination, improved the filter, made the display of articles by filters

--- frontend/src/app/shared/services/article.service.ts
+++ frontend/src/app/shared/services/article.service.ts
@@ -4,6 +4,7 @@ import {ArticleType} from "../../../types/article.type";
 import {environment} from "../../../environments/environment";
 import {Observable} from "rxjs";
 import {CategoryType} from "../../../types/category.type";
+import {ActiveParamsType} from "../../../types/active-params.type";
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +16,8 @@ export class ArticleService {
   getPopularArticles(): Observable<ArticleType[]>{
     return this.http.get<ArticleType[]>(environment.api + 'articles/top');
   }
-  getArticles(): Observable<{ count: number, pages: number, items: ArticleType[] }>{
-    return this.http.get<{ count: number, pages: number, items: ArticleType[] }>(environment.api + 'articles');
+  getArticles(params: ActiveParamsType): Observable<{ count: number, pages: number, items: ArticleType[] }>{
+    return this.http.get<{ count: number, pages: number, items: ArticleType[] }>(environment.api + 'articles', {params});
   }
   getCategories(): Observable<CategoryType[]>{
     return this.http.get<CategoryType[]>(environment.api + 'categories');
--- frontend/src/app/views/blog/articles/articles.component.ts
+++ frontend/src/app/views/blog/articles/articles.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 import {ArticleService} from "../../../shared/services/article.service";
 import {ArticleType} from "../../../../types/article.type";
 import {CategoryType} from "../../../../types/category.type";
 import {ActiveParamsType} from "../../../../types/active-params.type";
 import {ActivatedRoute, Router} from "@angular/router";
 import {AppliedFilterType} from "../../../../types/applied-filter.type";
+import {debounceTime} from "rxjs";
 
 @Component({
   selector: 'app-articles',
@@ -17,19 +18,26 @@ export class ArticlesComponent implements OnInit {
   filtersOpen: boolean = false;
   activeParams: ActiveParamsType = {categories: []};
   appliedFilters: AppliedFilterType[] = [];
+  pages: number[] = [];
+
+  @ViewChild('blogFilters') blogFilters!: ElementRef;
 
   constructor(private articleService: ArticleService,
               private router: Router,
               private activatedRoute: ActivatedRoute) {
   }
 
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const blogFilterElem = this.blogFilters.nativeElement as HTMLElement;
+    console.log()
+    if (!blogFilterElem.contains(target)) {
+      this.filtersOpen = false;
+    }
+  }
+
   ngOnInit() {
-
-    this.articleService.getArticles()
-      .subscribe(data => {
-        this.articles = data.items;
-      });
-
     this.articleService.getCategories()
       .subscribe(data => {
         this.categories = data.map(item => {
@@ -37,7 +45,11 @@ export class ArticlesComponent implements OnInit {
           return item;
         });
 
-        this.activatedRoute.queryParams.subscribe(params => {
+        this.activatedRoute.queryParams
+          .pipe(
+            debounceTime(500)
+          )
+          .subscribe(params => {
           const activeParams: ActiveParamsType = {categories: []};
           if (params.hasOwnProperty('categories')) {
             activeParams.categories = Array.isArray(params['categories']) ? params['categories'] : [params['categories']];
@@ -58,7 +70,6 @@ export class ArticlesComponent implements OnInit {
 
           this.appliedFilters = [];
           this.activeParams.categories.forEach(url => {
-
             const foundCategory = this.categories.find(item => item.url === url);
             if (foundCategory) {
               this.appliedFilters.push({
@@ -68,7 +79,14 @@ export class ArticlesComponent implements OnInit {
             }
           });
 
-
+          this.articleService.getArticles(this.activeParams)
+            .subscribe(data => {
+              this.pages = [];
+              for (let i = 1; i <= data.pages; i++) {
+                this.pages.push(i);
+              }
+              this.articles = data.items;
+            });
         });
       });
   }
@@ -78,6 +96,9 @@ export class ArticlesComponent implements OnInit {
   }
 
   updateFilterParam(url: string, isSearching: boolean) {
+    this.filtersOpen = false;
+    this.activeParams.page = 1;
+
     this.categories.filter(item => {
       if (item.url === url) {
         item.isSearching = !item.isSearching;
@@ -99,8 +120,6 @@ export class ArticlesComponent implements OnInit {
     this.router.navigate(['/blog'], {
       queryParams: this.activeParams,
     })
-
-
   }
 
   removeAppliedFilter(appliedFilter: AppliedFilterType) {
@@ -110,8 +129,38 @@ export class ArticlesComponent implements OnInit {
         item.isSearching = false;
       }
     });
+    this.activeParams.page = 1;
     this.router.navigate(['/blog'], {
       queryParams: this.activeParams
     });
   }
+
+  openPage(page: number) {
+    this.activeParams.page = page;
+    this.router.navigate(['/blog'], {
+      queryParams: this.activeParams
+    });
+  }
+
+  openPrevPage(){
+    if(this.activeParams.page && this.activeParams.page > 1){
+      this.activeParams.page--;
+      this.router.navigate(['/blog'], {
+        queryParams: this.activeParams
+      });
+    }
+
+  }
+  openNextPage(){
+    if(!this.activeParams.page){
+      this.activeParams.page = 1;
+    }
+    if(this.activeParams.page && this.activeParams.page < this.pages.length){
+      this.activeParams.page++;
+      this.router.navigate(['/blog'], {
+        queryParams: this.activeParams
+      });
+    }
+  }
+
 }
